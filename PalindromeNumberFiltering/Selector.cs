5e99be4fb243be3897d46c26865cd2aba63a5367
Hovsep <hovsephakobyan04@gmail.com>
2024-09-06 16:28:58 +0400

    done

--- PalindromeNumberFiltering/Selector.cs
+++ PalindromeNumberFiltering/Selector.cs
@@ -1,62 +1,121 @@
-﻿using System.Collections.Concurrent;
+using System.Collections.Concurrent;
 
-namespace PalindromeNumberFiltering;
-
-/// <summary>
-/// A static class containing methods for filtering palindrome numbers from a collection of integers.
-/// </summary>
-public static class Selector
+namespace PalindromeNumberFiltering
 {
     /// <summary>
-    /// Retrieves a collection of palindrome numbers from the given list of integers using sequential filtering.
+    /// A static class containing methods for filtering palindrome numbers from a collection of integers.
     /// </summary>
-    /// <param name="numbers">The list of integers to filter.</param>
-    /// <returns>A collection of palindrome numbers.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when the input list 'numbers' is null.</exception>
-    public static IList<int> GetPalindromeInSequence(IList<int>? numbers)
+    public static class Selector
     {
-        throw new NotImplementedException();
-    }
+        /// <summary>
+        /// Retrieves a collection of palindrome numbers from the given list of integers using sequential filtering.
+        /// </summary>
+        /// <param name="numbers">The list of integers to filter.</param>
+        /// <returns>A collection of palindrome numbers.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the input list 'numbers' is null.</exception>
+        public static IList<int> GetPalindromeInSequence(IList<int>? numbers)
+        {
+            ArgumentNullException.ThrowIfNull(numbers);
+            return numbers.Where(number => IsPalindrome(number)).ToList();
+        }
 
-    /// <summary>
-    /// Retrieves a collection of palindrome numbers from the given list of integers using parallel filtering.
-    /// </summary>
-    /// <param name="numbers">The list of integers to filter.</param>
-    /// <returns>A collection of palindrome numbers.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when the input list 'numbers' is null.</exception>
-    public static IList<int> GetPalindromeInParallel(IList<int> numbers)
-    {
-        throw new NotImplementedException();
-    }
+        /// <summary>
+        /// Retrieves a collection of palindrome numbers from the given list of integers using parallel filtering.
+        /// </summary>
+        /// <param name="numbers">The list of integers to filter.</param>
+        /// <returns>A collection of palindrome numbers.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the input list 'numbers' is null.</exception>
+        public static IList<int> GetPalindromeInParallel(IList<int> numbers)
+        {
+            var palindromeNumbers = new ConcurrentBag<int>();
+            _ = Parallel.ForEach(numbers, number =>
+             {
+                 if (IsPalindrome(number))
+                 {
+                     palindromeNumbers.Add(number);
+                 }
+             });
 
-    /// <summary>
-    /// Checks whether the given integer is a palindrome number.
-    /// </summary>
-    /// <param name="number">The integer to check.</param>
-    /// <returns>True if the number is a palindrome, otherwise false.</returns>
-    private static bool IsPalindrome(int number)
-    {
-        throw new NotImplementedException();
-    }
+            return palindromeNumbers.ToList();
+        }
 
-    /// <summary>
-    /// Recursively checks whether a positive number is a palindrome.
-    /// </summary>
-    /// <param name="number">The positive number to check.</param>
-    /// <param name="divider">The divider used in the recursive check.</param>
-    /// <returns>True if the positive number is a palindrome, otherwise false.</returns>
-    private static bool IsPositiveNumberPalindrome(int number, int divider)
-    {
-        throw new NotImplementedException();
-    }
+        /// <summary>
+        /// Checks whether the given integer is a palindrome number.
+        /// </summary>
+        /// <param name="number">The integer to check.</param>
+        /// <returns>True if the number is a palindrome, otherwise false.</returns>
+        private static bool IsPalindrome(int number)
+        {
+            if (number < 0)
+            {
+                return false;
+            }
 
-    /// <summary>
-    /// Gets the number of digits in the given integer.
-    /// </summary>
-    /// <param name="number">The integer to count digits for.</param>
-    /// <returns>The number of digits in the integer.</returns>
-    private static byte GetLength(int number)
-    {
-        throw new NotImplementedException();
+            return IsPositiveNumberPalindrome(number, (int)Math.Pow(10, GetLength(number) - 1));
+        }
+
+        /// <summary>
+        /// Recursively checks whether a positive number is a palindrome.
+        /// </summary>
+        /// <param name="number">The positive number to check.</param>
+        /// <param name="divider">The divider used in the recursive check.</param>
+        /// <returns>True if the positive number is a palindrome, otherwise false.</returns>
+        private static bool IsPositiveNumberPalindrome(int number, int divider)
+        {
+            var length = GetLength(number);
+            if (number < 10)
+            {
+                return true;
+            }
+
+            int left = number / divider;
+            int right = number % 10;
+            var nextNumber = (number % divider) / 10;
+            if (left == right)
+            {
+                return IsPositiveNumberPalindrome(nextNumber, divider / 100);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets the number of digits in the given integer.
+        /// </summary>
+        /// <param name="number">The integer to count digits for.</param>
+        /// <returns>The number of digits in the integer.</returns>
+        private static byte GetLength(int number)
+        {
+            if (number < 0)
+            {
+                number = -number;
+            }
+
+            switch (number)
+            {
+                case >= 1000000000:
+                    return 10;
+                case >= 100000000:
+                    return 9;
+                case >= 10000000:
+                    return 8;
+                case >= 1000000:
+                    return 7;
+                case >= 100000:
+                    return 6;
+                case >= 10000:
+                    return 5;
+                case >= 1000:
+                    return 4;
+                case >= 100:
+                    return 3;
+                case >= 10:
+                    return 2;
+                default:
+                    return 1;
+            }
+        }
     }
 }
